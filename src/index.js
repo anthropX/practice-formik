8fd5acbc17af151bcd367f92943f7a8792368b45
Vimal Patel <94vimalpatel@gmail.com>
2020-11-25 02:36:48 +0530

    feat: reduce boilerplate with Formik components

--- src/index.js
+++ src/index.js
@@ -1,50 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { useFormik } from "formik";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 import { object, string } from "yup";
 import "./styles.css";
 
 const App = () => {
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      firstName: "",
-      lastName: "",
-    },
-    validationSchema: object({
-      email: string().email("Invalid email").required("Required"),
-      firstName: string()
-        .max(15, "Mustn't exceed 15 char")
-        .required("Required"),
-      lastName: string().max(15, "Mustn't exceed 15 char").required("Required"),
-    }),
-    onSubmit: (values) => console.log(values),
-  });
   return (
-    <form noValidate onSubmit={formik.handleSubmit}>
-      <label htmlFor="email">First Name</label>
-      <input
-        id="firstName"
-        type="text"
-        {...formik.getFieldProps("firstName")}
-      />
-      {formik.touched.firstName && formik.errors.firstName ? (
-        <div>{formik.errors.firstName}</div>
-      ) : null}
+    <Formik
+      initialValues={{ email: "", firstName: "", lastName: "" }}
+      validationSchema={object({
+        email: string().email("Invalid email").required("Required"),
+        firstName: string()
+          .max(15, "Mustn't exceed 15 char")
+          .required("Required"),
+        lastName: string()
+          .max(15, "Mustn't exceed 15 char")
+          .required("Required"),
+      })}
+      onSubmit={(values) => console.log(values)}
+    >
+      {(formik) => (
+        <Form noValidate>
+          <label htmlFor="firstName">First Name</label>
+          <Field name="firstName" type="text" />
+          <ErrorMessage name="firstName" />
 
-      <label htmlFor="email">Last Name</label>
-      <input id="lastName" type="text" {...formik.getFieldProps("lastName")} />
-      {formik.touched.lastName && formik.errors.lastName ? (
-        <div>{formik.errors.lastName}</div>
-      ) : null}
+          <label htmlFor="lastName">Last Name</label>
+          <Field name="lastName" type="text" />
+          <ErrorMessage name="lastName" />
 
-      <label htmlFor="email">Email</label>
-      <input id="email" type="email" {...formik.getFieldProps("email")} />
-      {formik.touched.email && formik.errors.email ? (
-        <div>{formik.errors.email}</div>
-      ) : null}
-      <button type="submit">Submit</button>
-    </form>
+          <label htmlFor="email">Email</label>
+          <Field name="email" type="email" />
+          <ErrorMessage name="email" />
+          <button type="submit">Submit</button>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
